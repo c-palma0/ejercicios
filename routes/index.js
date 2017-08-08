c3eb3a742fb4e381ce5c7c69b244af910e8929bb
Carolina <c.palma.p0@gmail.com>
2017-08-08 10:27:21 -0500

    node, go , insertar a postgresql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+});
+/*
+router.get('/test/:id', function(req, res, next) {
+  res.render('test', {output: req.params.id});
+});*/
+
+var options = {
+  host: '127.0.0.1',
+  path: '/',
+  port: '8081',
+ 
+};
+
+
+router.post('/go', function(req, res, next) {
+  
+          callback = function(response) {
+            var str = ''
+            response.on('data', function (chunk) {
+              str += chunk;
+            });
+
+            response.on('end', function () {
+              console.log(str);
+            });
+          }
+
+          var req = http.request(options, callback);
+          req.end();
+ // res.redirect('/test/');
 });
 
 module.exports = router;
+
+
+
